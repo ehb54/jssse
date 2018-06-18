53ec174c6fc6c35c4fb8749c9647a8303076d86b
Nayman Leung <naymansleung@gmail.com>
2018-06-18 15:18:21 -0400

    revised functionality for load, wrote skeleton functions for options, update, and out methods

--- JS Widget Basics/javascript.js
+++ JS Widget Basics/javascript.js
@@ -1,7 +1,7 @@
 
 $( function() {
-    // the widget definition, where "custom" is the namespace,
-    // "jspdb" the widget name
+//     the widget definition, where "custom" is the namespace,
+//     "jspdb" the widget name
     $.widget( "custom.jspdb", {
       // default options
       options: {
@@ -69,17 +69,37 @@ $( function() {
     },
     load: function(obj, data, cb){
       //1. Parse/match file reference
-      var type = /^([a-zA-z]){4,6}:\/\//;
+      var type = /^([a-zA-z]){4,5}:\/\//;
       var res = type.exec(data);
+      console.log(res);
+      //assume local data?
+      if(res==null){
+        obj.data = data;
+        console.log(obj);
+      }
       //check address type (file vs https)
-      if(res.equals("file")){
-        //load file from local machine
-      }
-      else if(res.equals("https")){
+      else if(res[0]==="https://"){
         //retrieve file from pdb database
-      }
+        $.get(data, function(info){
+          //do something with info (assign to data property)
+          obj.data = info;
+          //console.log(info);
+        })
+        .fail(function(err){
+          console.log("error occured!");
+      });
     }
-  };
+  },
+  out: function(obj,options){
+    if(options.format==='pdb'){
+      //call fn/script to parse obj.data to pdb
+    }
+    //handle other formats
+  },
+  update: function(obj, options, cb){
+    //update options properties
+  }
+};
 
 
 /*  make sure DOM is properly loaded before manipulating (changing div into
@@ -101,4 +121,31 @@ window.onload=function(){
   //add event listeners
   jspdb.listen(obj1);
   jspdb.listen(obj2);
+
+  //handling local files
+  function handleFileSelect(e) {
+    var files = e.target.files; // FileList object (list of File objects)
+    var file = files[0];
+    var reader = new FileReader();
+    reader.onload = function (evt){
+      //grabs info
+      var info = evt.target.result;
+      //create jspdb object
+      var obj3 = jspdb.create('fakediv',{});
+      console.log(info);
+      jspdb.load(obj3, info, function(err){if(err){throw err;}});
+    }
+    reader.readAsText(file);
+
+
+  }
+  //add event listener for when the user selects a file
+  document.getElementById('files').addEventListener('change', handleFileSelect, false);
+  //do something with data
+
+  //other stuff
+  jspdb.load(obj1, "https://files.rcsb.org/view/5W4C.pdb", function(err){
+    if(err){throw err;}
+    console.log("success");
+  });
 }
