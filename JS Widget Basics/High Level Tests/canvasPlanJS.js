0d962cd11d1311bfc081e88496dd5ae887d0de34
Nayman Leung <naymansleung@gmail.com>
2018-07-12 11:31:12 -0400

    added CSS grid box demo

--- JS Widget Basics/High Level Tests/canvasPlanJS.js
+++ JS Widget Basics/High Level Tests/canvasPlanJS.js
@@ -1,23 +1,25 @@
 $( function() {
+  //code to open dialog
   // $( "#dialog" ).dialog({ autoOpen: false });
   // $( "#opener" ).click(function() {
   //   $( "#dialog" ).dialog( "open" );
   // });
 
-  $('.SSE')
-  .draggable({
-    handle: "h2",
-    snap: true,
-    containment: 'parent'
-    // ,snap: '.ui-widget-header'
-  });
-  $('#build-canvas')
-  .draggable({
-    handle: "h2",
-    snap: true,
-    containment: 'parent'
-    // ,snap: '.ui-widget-header'
-  });
+  //UNCOMMENT THESE TO ENABLE JQUERY (with CSS, the elements don't stick to grid-defined columns )
+  // $('.SSE')
+  // .draggable({
+  //   handle: "h2",
+  //   snap: true,
+  //   containment: 'parent'
+  //   // ,snap: '.ui-widget-header'
+  // });
+  // $('#build-canvas')
+  // .draggable({
+  //   handle: "h2",
+  //   snap: true,
+  //   containment: 'parent'
+  //   // ,snap: '.ui-widget-header'
+  // });
 
   //UNCOMMENT THESE TWO LINES TO PLAY AROUND WITH DIALOGS (position not inherently fixed - they are spawned on top)
   // $('.SSE').dialog();
